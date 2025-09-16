b52b9b5db79d60ddd61f8e871e2c20b5812c1b38
Pedro Mendes <pedrohenriquecamposmendes@hotmail.com>
2025-09-16 13:38:17 -0300

    feat: create the fastify error handler

---
+++ server/src/infra/http/error-handler.ts
@@ -0,0 +1,38 @@
+import type { FastifyInstance } from 'fastify'
+import { ZodError } from 'zod'
+import { AlreadyRegisteredEmailError, ResourceNotFoundError } from './util/errors'
+
+type FastifyErrorHandler = FastifyInstance['errorHandler']
+
+export const errorHandler: FastifyErrorHandler = (error, _request, reply) => {
+  console.error({ error })
+  
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      name: error.name,
+      message: `Error during Validation`,
+      errors: error.flatten,
+    })
+  }
+
+  if (error instanceof AlreadyRegisteredEmailError) {
+    return reply.status(401).send({
+      name: error.name,
+      cause: error.cause,
+      message: error.message,
+    })
+  }
+
+  if (error instanceof ResourceNotFoundError) {
+    return reply.status(404).send({
+      name: error.name,
+      cause: error.cause,
+      message: error.message,
+    })
+  }
+
+  return reply.status(500).send({
+    message: 'Internal Server Error',
+  })
+}
+
