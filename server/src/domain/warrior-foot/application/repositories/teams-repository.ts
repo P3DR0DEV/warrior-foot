912354fd7bb7a4744b11ebaab4779ce2da3ced72
Pedro Mendes <pedrohenriquecamposmendes@hotmail.com>
2025-09-18 17:52:56 -0300

    feat: create domain use-cases

--- server/src/domain/warrior-foot/application/repositories/teams-repository.ts
+++ server/src/domain/warrior-foot/application/repositories/teams-repository.ts
@@ -3,6 +3,7 @@ import type { Team } from '#domain/warrior-foot/enterprise/entities/team.ts'
 export interface TeamsRepository {
   findById(id: string): Promise<Team | null>
   findByLeagueId(leagueId: string): Promise<Team[]>
+  createMany(teams: Team[]): Promise<void>
 
   create(team: Team): Promise<void>
 }
