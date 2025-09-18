6d22fe94e2b0401e9b521b673df29667494459b9
Pedro Mendes <pedrohenriquecamposmendes@hotmail.com>
2025-09-18 16:29:28 -0300

    feat: create prisma mappers and repository

---
+++ server/src/infra/database/prisma/mappers/prisma-teams-mapper.ts
@@ -0,0 +1,29 @@
+import { UniqueEntityId } from '#core/entities/unique-entity-id.ts'
+import { Team } from '#domain/warrior-foot/enterprise/entities/team.ts'
+import type { Prisma, Team as PrismaTeam } from '#prisma/index.js'
+
+export class PrismaTeamsMapper {
+  static toDomain(raw: PrismaTeam): Team {
+    const leagueId = new UniqueEntityId(raw.leagueId)
+
+    return Team.create({
+      name: raw.name,
+      primaryColor: raw.primaryColor,
+      secondaryColor: raw.secondaryColor,
+      division: raw.division,
+      leagueId,
+      createdAt: raw.createdAt,
+    })
+  }
+
+  static toPersistence(team: Team): Prisma.TeamUncheckedCreateInput {
+    return {
+      id: team.id.toValue(),
+      name: team.name,
+      primaryColor: team.primaryColor,
+      secondaryColor: team.secondaryColor,
+      division: team.division,
+      leagueId: team.leagueId.toValue(),
+    }
+  }
+}
