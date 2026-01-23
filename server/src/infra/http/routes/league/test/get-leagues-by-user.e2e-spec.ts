9d7361e2e75d1697259f3feda50c026657c28755
Pedro Mendes <pedrohenriquecamposmendes@hotmail.com>
2026-01-23 14:05:21 -0300

    tests: add auth for e2e tests

--- server/src/infra/http/routes/league/test/get-leagues-by-user.e2e-spec.ts
+++ server/src/infra/http/routes/league/test/get-leagues-by-user.e2e-spec.ts
@@ -1,6 +1,7 @@
 import supertest from 'supertest'
 import { app } from '#infra/http/app-test.ts'
 import { db } from '#infra/lib/drizzle.ts'
+import { makeAuthenticatedUser } from '#test/factories/make-authenticated-user.ts'
 import { LeagueFactory } from '#test/factories/make-league.ts'
 import { UserFactory } from '#test/factories/make-user.ts'
 
@@ -24,7 +25,11 @@ describe('Test Get Leagues By User (E2E)', () => {
       await leagueFactory.createLeague({ userId: user.id })
     }
 
-    const response = await supertest(app.server).get(`/leagues/${user.id}/leagues`)
+    const { token } = await makeAuthenticatedUser({ user })
+
+    const response = await supertest(app.server)
+      .get(`/leagues/${user.id}/leagues`)
+      .set('Authorization', `Bearer ${token}`)
 
     expect(response.statusCode).toEqual(200)
 
